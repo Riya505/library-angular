b128b62e563514b1612928d6aaf2b53d5ddd2683
Riya505 <riyapk505@gmail.com>
2022-10-20 06:28:09 +0530

    new

---
+++ src/app/viewbooks/viewbooks.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-viewbooks',
+  templateUrl: './viewbooks.component.html',
+  styleUrls: ['./viewbooks.component.css']
+})
+export class ViewbooksComponent implements OnInit {
+
+  constructor(private myapi:ApiService) { 
+    this.fetchData()
+  }
+  fetchData=()=>{
+    this.myapi.viewBooks().subscribe(
+      (data)=>{
+        this.viewData=data
+      }
+    )
+  }
+
+  viewData:any=[]
+
+  ngOnInit(): void {
+  }
+
+}
