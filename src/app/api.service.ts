cbad9afc660051bd12bbfe6a850c10ba4ffc233b
Riya505 <riyapk505@gmail.com>
2022-10-20 06:06:41 +0530

    new

---
+++ src/app/api.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+  viewBooks=()=>{
+    return this.http.get("http://localhost:8080/viewbooks")
+  }
+  
+}
